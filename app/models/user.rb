b9f0805225536bad5c9806a0dfd428f04ef5eef5
Yogendra <yogendra.v@cisinlabs.com>
2013-12-31 15:05:31 +0530

    updated login+reg

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :image, :address, :address_latitude, :address_longitude, :address_locality, :address_country
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :address, :address_latitude, :address_longitude, :address_locality, :address_country, :pictures_attributes
+  has_many :pictures, :as => :resource, :dependent => :destroy
+  accepts_nested_attributes_for :pictures, :allow_destroy => true
   # attr_accessible :title, :body
 end
