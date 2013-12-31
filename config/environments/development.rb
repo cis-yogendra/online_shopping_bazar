9bb347d3bd3771a32beeac9185ccff1c79b53f20
Yogendra <yogendra.v@cisinlabs.com>
2013-12-31 18:49:37 +0530

    updated sass

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,17 @@ Onlinebazar::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #Setting localhost for mailer
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "yogendra.ogn@gmail.com",
+    password: "constant@"
+  }
 end
