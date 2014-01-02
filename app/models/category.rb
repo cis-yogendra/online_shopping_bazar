51ed47d4c2c68cad543c5591b9aecaedac00b898
Yogendra <yogendra.v@cisinlabs.com>
2014-01-02 15:30:46 +0530

    main navigation in progress

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   attr_accessible :name, :parent_id
   validates :name, :presence => true
-  validates_uniqueness_of :name, :message => "%{value} has been taken"
+  # validates_uniqueness_of :name, :message => "%{value} has been taken"
   has_many :products, :dependent => :destroy
   has_many :sub_categories, :dependent => :destroy, :class_name => "Category", :foreign_key => :parent_id
   belongs_to :parent, :class_name => "Category"
